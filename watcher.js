4a81c1a0a8b8c200812033e427e8b309a204fef8
DivNectar <divnectar.design@gmail.com>
2020-05-09 12:49:41 -0400

    first commit

---
+++ watcher.js
@@ -0,0 +1,41 @@
+let filewatcher = require("filewatcher");
+let fs = require("fs");
+let path = require("path");
+
+let watchFolder = path.join(__dirname, "files");
+let fsTimeout;
+
+fs.watch(watchFolder, (eventType, filename) => {
+  let currentDay = new Date();
+
+  let month = currentDay.getMonth().toString();
+  let day = currentDay.getDate().toString();
+  let year = currentDay.getFullYear().toString();
+
+  let todaysFolderPath = path.join(watchFolder, year, month, day);
+
+  if (!fsTimeout) {
+    fsTimeout = setTimeout(function () {
+      fsTimeout = null;
+    }, 5000); // give 5 seconds for multiple events
+    if (filename) {
+      console.log(filename);
+      let fileName = path.basename(filename);
+      if (fs.existsSync(todaysFolderPath)) {
+        console.log("folder already exists");
+        fs.renameSync(
+          path.join(watchFolder, filename),
+          path.join(todaysFolderPath, fileName)
+        );
+      } else {
+        console.log("folder does not exist yet");
+        fs.mkdirSync(todaysFolderPath, { recursive: true });
+        fs.renameSync(
+          path.join(watchFolder, filename),
+          path.join(todaysFolderPath, fileName)
+        );
+      }
+    }
+  }
+
+});
